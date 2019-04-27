fe4e0ed634842fe8a4c0a0c8fce13150c8bcbc94
Raken <hammed.raken123@gmail.com>
2019-04-27 17:00:29 +0100

    add create user feature

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,4 +1,9 @@
 import { Request, Response } from "express";
+import Web3 from 'web3';
+import { User } from "../models/User";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import * as authenticationServices from "../services/authenticationServices";
 
 /**
  * userController.list()
@@ -20,7 +25,7 @@ export const list = async (req: Request, res: Response) => {
 export const show = async (req: Request, res: Response) => {
   const id = req.params.id;
   try {
-
+    authenticationServices.extractTokenAndVerify(req.headers.authorization);
     res.status(200).json(id);
   } catch (err) {
     res.status(500).json({
@@ -34,9 +39,22 @@ export const show = async (req: Request, res: Response) => {
  * userController.create()
  */
 export const create = async (req: Request, res: Response) => {
+  const web3 = new Web3(Web3.givenProvider || 'ws://localhost:7545', null, {});
+  const account = web3.eth.accounts.create();
+  const user = new User({
+    accountPrivateKey: account.privateKey,
+    accountAddress: account.address,
+    email: req.body.email,
+    password: hashPassword(req.body.password),
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phoneNumber: req.body.phoneNumber,
+    birthDate: req.body.birthDate,
+    nationalId: req.body.nationalId,
+  });
   try {
-    res.status(201).json();
-    res.sendStatus(201);
+    const dbUser = await user.save();
+    res.status(201).json(dbUser);
   } catch (err) {
     res.status(500).json({
       error: err,
@@ -65,3 +83,9 @@ export const remove = async (req: Request, res: Response) => {
     });
   }
 };
+
+function hashPassword(password: string): string {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+}
